1ec7581a1bd06ee8fbf6b2873aec9c773424285e
Jakub Jurczyk <pandetthe@gmail.com>
2024-08-18 19:27:48 +0200

    Small fixes to the event

--- API/Events/Provider/CheckValidWithExplanationEvent.cs
+++ API/Events/Provider/CheckValidWithExplanationEvent.cs
@@ -5,10 +5,10 @@ namespace Alpalis.UtilityServices.API.Events.Provider
 {
     public class CheckValidWithExplanationEvent(ValidateAuthTicketResponse_t callback, bool isValid, string explanation) : Event
     {
-        public ValidateAuthTicketResponse_t Callback { get; } = callback;
+        public ValidateAuthTicketResponse_t Callback { get; set; } = callback;
 
-        public bool IsValid { get; } = isValid;
+        public bool IsValid { get; set; } = isValid;
 
-        public string Explanation { get; } = explanation;
+        public string Explanation { get; set; } = explanation;
     }
 }
